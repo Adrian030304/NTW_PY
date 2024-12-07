042ea59764c282e30c269f582fd1847c5741bcfb
Adrian030304 <adrianromanenco1@gmail.com>
2024-12-07 17:16:19 +0200

    client connection accept and connection success

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('127.0.0.1',55555))
+s.bind(('127.0.0.1',5500))
 s.listen()
+
+while True:
+    client, address = s.accept() #accepts the connection
+    print("Connected to: {}".format(address))
+    client.send("You are connected!".encode())
+    client.close()
